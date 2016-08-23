a2046ef734ebe145476d1b5c6b8c93c6a2131adf
Gordon <civfactions@gmail.com>
2016-08-23 15:21:36 -0500

    Make spigot plugin for loading classes

---
+++ src/main/java/com/civfactions/SabreCore/SabreCorePlugin.java
@@ -0,0 +1,12 @@
+package com.civfactions.SabreCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * This class gets Spigot to load the SabreCore class paths
+ * @author Gordon
+ *
+ */
+public class SabreCorePlugin extends JavaPlugin {
+
+}
